f5527610ea435e07003690e90ff98f01473148e0
Tajinderpal Singh <bajwataj@gmail.com>
2021-12-30 19:32:25 -0500

    Add files via upload
    Calculate the sum of all numbers from 1 to a given number

---
+++ Sum to Given Number.py
@@ -0,0 +1,8 @@
+'''Calculate the sum of all numbers from 1 to a given number'''
+number= int(input("Enter the Given number  to sum from 1: "))
+total=0
+for i in range(1,number+1):
+    total+=i
+print(total)
+
+
