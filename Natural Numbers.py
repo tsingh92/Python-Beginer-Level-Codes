aca62a959a90df3212162526810472244ceb66a4
Tajinderpal Singh <bajwataj@gmail.com>
2021-12-30 19:33:48 -0500

    Update Natural Numbers.py

--- Natural Numbers.py
+++ Natural Numbers.py
@@ -1,13 +1,13 @@
 #Print Natural Numbers from 1 to User Choice While Loop
 print("Welcome to Printing Natural Number from 1 to Your choice number.")
-number= int(input("Enter the number: "))
-number_in_list=[]
+number= int(input("Enter the number: "))  #change string to integer
+number_in_list=[]   #Empty List
 num=1
 while num<=number:
-    number_in_list.append(num)
+    number_in_list.append(num)  #add numbers in list
     print(num)
     num+=1
     
 print("Natural Number in list as follows:")
-print(number_in_list)
+print(number_in_list) 
 
